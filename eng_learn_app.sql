3a90e82d2996a3676453ae01f68cd43a4ba4d3da
huuluc10 <lucnguyenhuu91@gmail.com>
2024-03-08 09:11:34 +0700

    docs: rename file

--- eng_learn_app.sql
+++ eng_learn_app.sql
@@ -19,7 +19,7 @@ create table if not exists media_type
 create table if not exists media
 (
     media_id int primary key auto_increment,
-    media_name varchar(30) not null,
+    media_name varchar(100) not null,
     media_type_id tinyint not null,
     url text default null,
     foreign key(media_type_id) references media_type(media_type_id)
@@ -30,6 +30,7 @@ create table if not exists user
     username varchar(30) primary key,
     full_name varchar(70) not null,
     date_of_birth date not null,
+    gender boolean default 1,
     email varchar(50) null,
     password varchar(100) not null,
     avatar int not null,
@@ -73,8 +74,8 @@ create table if not exists level
 
 create table if not exists lesson
 (
-    lesson_id tinyint primary key auto_increment,
-    lesson_name varchar(30) not null,
+    lesson_id int primary key auto_increment,
+    lesson_name varchar(100) not null,
     topic_id tinyint not null,
     content int not null,
     lesson_experience int not null,
@@ -97,7 +98,7 @@ create table if not exists user_mission
 create table if not exists user_lesson
 (
     username varchar(30) not null,
-    lesson_id tinyint not null,
+    lesson_id int not null,
     primary key(username, lesson_id),
     foreign key(username) references user(username),
     foreign key(lesson_id) references lesson(lesson_id)
@@ -105,10 +106,12 @@ create table if not exists user_lesson
 
 create table if not exists exam
 (
-    exam_id tinyint primary key auto_increment,
+    exam_id int primary key auto_increment,
     exam_name varchar(30) not null,
     exam_experience int not null,
     exam_level tinyint not null,
+    topic_id tinyint not null,
+    foreign key(topic_id) references topic(topic_id),
     foreign key(exam_level) references level(level_id)
 );
 
@@ -124,8 +127,8 @@ create table if not exists question
     question_content varchar(100) not null,
     question_type_id tinyint not null,
     belong_to ENUM ('LESSON', 'EXAM') default 'LESSON',
-    lesson_id tinyint null,
-    exam_id tinyint null,
+    lesson_id int null,
+    exam_id int null,
     answer int not null,
     foreign key (answer) references media(media_id),
     foreign key(exam_id) references exam(exam_id),
@@ -151,11 +154,10 @@ insert into level(level_name, experience_threshold) values ('Beginner', 0), ('El
                                                            ('Upper Intermediate', 2000), ('Advanced', 4500);
 
 insert into mission_daily(mission_name, mission_content, mission_experience) values
-                                                                                 ('Daily Login', 'Login to the app', 10),
                                                                                  ('Daily Learning', 'Learn a lesson', 20),
                                                                                  ('Daily Practice', 'Practice a lesson', 30);
 
-insert into topic(topic_name) values ('Word - Từ vựng'), ('Phonetics - Ngữ âm'),('Verb - Động từ'),('Tenses - Thì'),
+insert into topic(topic_name) values ('Word - Từ vựng'), ('Greeting - Lời Chào'),('Verb - Động từ'), ('Phonetics - Ngữ âm'), ('Tenses - Thì'),
                                      ('Tag question - Câu hỏi đuôi'), ('Prepositions - Giới từ'),
                                      ('Adjectives - Tính từ'), ('Adverbs - Trạng từ'), ('Nouns - Danh từ'),
                                      ('Pronouns - Đại từ'), ('Articles - Mạo từ'), ('Quantifiers - Từ chỉ số lượng'),
@@ -166,7 +168,51 @@ insert into topic(topic_name) values ('Word - Từ vựng'), ('Phonetics - Ngữ
                                      ('Proverbs - Tục ngữ');
 
 insert into media (media_id, media_name, media_type_id, url)
-value (1, 'Avartar', 1, 'images/avatars/avatar.jpg');
+value (1, 'Avartar', 1, 'images/avatars/avatar.jpg'),
+    (2, 'Deafault for male', 1, 'images/avatars/default-male.jpg'),
+    (3, 'Deafault for female', 1, 'images/avatars/default-female.jpg'),
+    (4, 'Fashion_Accessories.json', 4, 'topics/vocabulary/Fashion_Accessories.json'),
+    (5, 'Fashion_Accessories_1.json', 4, 'topics/vocabulary/Fashion_Accessories_1.json'),
+    (6, 'Type_Of_Hats.json', 4, 'topics/vocabulary/Type_Of_Hats.json'),
+    (7, 'Cloth_Summary.json', 4, 'topics/vocabulary/Cloth_Summary.json'),
+    (8, 'Men_clothes.json', 4, 'topics/vocabulary/Men_clothes.json'),
+    (9, 'Woman_clothes.json', 4, 'topics/vocabulary/Woman_clothes.json'),
+    (10, 'American_And_British_Words_For_Clothes.json', 4, 'topics/vocabulary/American_And_British_Words_For_Clothes.json'),
+    (11, 'Common_Action.json', 4, 'topics/vocabulary/Common_Action.json'),
+    (12, 'Common_Action_1.json', 4, 'topics/vocabulary/Common_Action_1.json'),
+    (13, 'Common_Action_2.json', 4, 'topics/vocabulary/Common_Action_2.json'),
+    (14, 'Family_Tree_Chart.json', 4, 'topics/vocabulary/Family_Tree_Chart.json'),
+    (15, 'General_Appearance.json', 4, 'topics/vocabulary/General_Appearance.json'),
+    (16, 'Adjectives_for_Describing_Age.json', 4, 'topics/vocabulary/Adjectives_for_Describing_Age.json'),
+    (17, 'Adjectives_To_Describe_Build.json', 4, 'topics/vocabulary/Adjectives_To_Describe_Build.json'),
+    (18, 'Adjectives_To_Describe_Personality_And_Character.json', 4, 'topics/vocabulary/Adjectives_To_Describe_Personality_And_Character.json'),
+    (19, 'Formal_Greetings.json', 4, 'topics/greeting/Formal_Greetings.json'),
+    (20, 'Informal_Greetings.json', 4, 'topics/greeting/Informal_Greetings.json'),
+    (21, 'Definition.json', 4, 'topics/verb/Definition.json'),
+    (22, 'Position_Of_Verb.json', 4, 'topics/verb/Position_Of_Verb.json');
 
 insert into user (username, password, email, date_of_birth, full_name, role_id, avatar, streak, experience)
-value ('admin', 'admin', null, now(), 'admin', 1, 1, 0, 0);
+value ('admin', 'admin', null, now(), 'admin', 1, 1, 0, 0),
+        ('huuluc10', 'huuluc10', 'lucnguyenhuu91@gmail.com', '2002-06-10', 'Nguyễn Hữu Lực', 2, 2, 0, 0);
+
+#     insert data into lesson table
+insert into lesson(lesson_name, topic_id, content, lesson_experience, level_id)
+value ('Fashion Accessories - Phụ kiện', 1, 4, 10, 1),
+    ('Fashion Accessories 2 - Phụ kiện', 1, 5, 12, 2),
+    ('Type Of Hats - Loại mũ', 1, 6, 10, 1),
+    ('Cloth Summary - Tổng kết quần áo', 1, 7, 12, 1),
+    ('Men clothes - Quần áo nam', 1, 8, 10, 1),
+    ('Woman clothes - Quần áo nữ', 1, 9, 10, 1),
+    ('American and british words for clothes - Từ ngữ Mỹ và Anh về quần áo', 1, 10, 20, 2),
+    ('Common Action - Hành động thông thường', 1, 11, 10, 1),
+    ('Common Action 1 - Hành động thông thường 1', 1, 12, 10, 1),
+    ('Common Action 2 - Hành động thông thường 2', 1, 13, 15, 2),
+    ('Family Tree Chart - Sơ đồ gia đình', 1, 14, 25, 2),
+    ('General Appearance - Ngoại hình', 1, 15, 10, 1),
+    ('Adjectives for Describing Age - Tính từ mô tả tuổi tác', 1, 16, 10, 1),
+    ('Adjectives To Describe Build - Tính từ mô tả dáng người', 1, 17, 10, 1),
+    ('Adjectives To Describe Personality And Character - Tính từ mô tả tính cách và tính tình', 1, 18, 20, 2),
+    ('Formal Greetings - Lời chào hỏi trang trọng', 2, 19, 10, 1),
+    ('Informal Greetings - Lời chào hỏi không trang trọng', 2, 20, 10, 1),
+    ('Definition - Định nghĩa', 3, 21, 10, 1),
+    ('Position Of Verb - Vị trí của động từ', 3, 22, 10, 1);
